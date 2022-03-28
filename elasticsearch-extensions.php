6602ac662f0b09a44023d5e54f5b75c43623134b
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2022-03-28 15:52:03 -0400

    Add basic phpcs and phpunit configs

---
+++ elasticsearch-extensions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:  Elasticsearch Extensions
+ * Plugin URI:   https://github.com/alleyinteractive/elasticsearch-extensions
+ * Description:  A WordPress plugin to make integrating sites with Elasticsearch easier.
+ * Author:       Alley
+ * Author URI:   https://alley.co/
+ * Text Domain:  elasticsearch-extensions
+ * Domain Path:  /languages
+ * Version:      0.1.0
+ *
+ * @package Elasticsearch_Extensions
+ */
+
+namespace Elasticsearch_Extensions;
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Elasticsearch Extensions Tests: Sample Test
+ *
+ * @package Elasticsearch_Extensions
+ * @subpackage Tests
+ */
+
+/**
+ * A sample test so we can run phpunit. Needs to be replaced by actual tests.
+ */
+class Test_Sample extends WP_UnitTestCase {
+
+	/**
+	 * A sample test.
+	 */
+	public function test_sample_test() {
+		$this->assertEquals( true, true );
+	}
+}
