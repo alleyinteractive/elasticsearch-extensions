262a171c87545aae07dc96f4b4bf105fdf159dbb
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2022-09-21 13:22:03 -0400

    Only load dev dependencies in a dev context

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,20 +6,21 @@
  * @subpackage Tests
  */
 
-define( 'ELASTICSEARCH_EXTENSIONS_TESTS_DIR', dirname( __DIR__ ) . '/tests/' );
+// Load Composer's autoloader.
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 Mantle\Testing\manager()
 	->before( function() {
 		// Define bootstrap helper functions.
-		require_once ELASTICSEARCH_EXTENSIONS_TESTS_DIR . '/includes/bootstrap-functions.php';
+		require_once __DIR__ . '/includes/bootstrap-functions.php';
 
 		// Load adapters bootstrap files.
-		require_once ELASTICSEARCH_EXTENSIONS_TESTS_DIR . '/includes/searchpress-bootstrap.php';
+		require_once __DIR__ . '/includes/searchpress-bootstrap.php';
 
 		// Loading Elasticsearch Extensions testcases.
-		require_once ELASTICSEARCH_EXTENSIONS_TESTS_DIR . '/includes/class-adapter-unit-test-case.php';
-		require_once ELASTICSEARCH_EXTENSIONS_TESTS_DIR . '/includes/class-searchpress-unit-test-case.php';
-		require_once ELASTICSEARCH_EXTENSIONS_TESTS_DIR . '/includes/class-vip-enterprise-search-unit-test-case.php';
+		require_once __DIR__ . '/includes/class-adapter-unit-test-case.php';
+		require_once __DIR__ . '/includes/class-searchpress-unit-test-case.php';
+		require_once __DIR__ . '/includes/class-vip-enterprise-search-unit-test-case.php';
 
 		uses( \SearchPress_Adapter_UnitTestCase::class)->in('adapters/searchpress' );
 		uses( \VIP_Enterprise_Search_Adapter_UnitTestCase::class)->in('adapters/vip-search' );
