a8d193edd34b9ba6ed0c310b19206274f7654ec7
Jake Foster <jake@alley.co>
2023-05-03 11:06:10 -0400

    add more comments

--- lib/adapters/class-vip-enterprise-search.php
+++ lib/adapters/class-vip-enterprise-search.php
@@ -165,9 +165,10 @@ class VIP_Enterprise_Search extends Adapter {
 					$formatted_args['post_filter']['bool']['must']
 				);
 			} elseif ( ! empty( $formatted_args['query']['match_all'] ) ) {
-				// Add filters to a match_all queries. This happens on secondary queries that use ep_integrate.
+				// This condition triggers on secondary queries that use ep_integrate.
+				// Adding filters to match_all queries requires converting it to a bool query with the match_all clause nested within.
 				$formatted_args['query']['bool']['must']['match_all'] = $formatted_args['query']['match_all'];
-				// Unset the original DSL, since this will use a `bool` query instead.
+				// Unset the original DSL, since this will use a bool query instead.
 				unset( $formatted_args['query']['match_all'] );
 				// Add bool query with filters.
 				$formatted_args['query']['bool']['filter'] = $formatted_args['post_filter']['bool']['must'];
