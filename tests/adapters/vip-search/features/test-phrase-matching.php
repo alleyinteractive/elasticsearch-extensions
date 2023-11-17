c7ecdc4e166282f185696918edf94325a598d235
Jake Foster <jake@alley.co>
2023-11-17 12:39:16 -0600

    add failing tests to vip-adapter

--- tests/adapters/vip-search/features/test-phrase-matching.php
+++ tests/adapters/vip-search/features/test-phrase-matching.php
@@ -6,6 +6,38 @@
  * @subpackage Tests
  */
 
-it( 'test version', function () {
-	$this->assertEquals( true, true );
+it( 'tests that phrase matching is disabled by default', function () {
+	$this->assertEquals( false, true );
+} );
+
+it( 'tests that phrase matching is enabled via function', function () {
+	add_action(
+		'elasticsearch_extensions_config',
+		function( $es_config ) {
+			$es_config->enable_phrase_matching();
+		}
+	);
+
+	$this->assertEquals( false, true );
+} );
+
+it( 'tests that phrase matching is disabled via function', function () {
+	// Enable on priority 10 (default priority).
+	add_action(
+		'elasticsearch_extensions_config',
+		function( $es_config ) {
+			$es_config->enable_phrase_matching();
+		}
+	);
+
+	// Called at a later priority to ensure that it happens after it is toggled on.
+	add_action(
+		'elasticsearch_extensions_config',
+		function( $es_config ) {
+			$es_config->enable_phrase_matching();
+		},
+		11
+	);
+
+	$this->assertEquals( false, true );
 } );
