410038dd7a4fb82ee85b0fec0fa7a626c6f4015a
Jake Foster <jake@alley.co>
2024-04-18 13:38:29 -0400

    add getter for phrase matching feature in controller

--- tests/adapters/vip-search/VipSearchPhraseMatchingTest.php
+++ tests/adapters/vip-search/VipSearchPhraseMatchingTest.php
@@ -7,30 +7,37 @@
  */
 use Elasticsearch_Extensions\Factory;
 
-it( 'tests that phrase matching matches phrase exactly', function () {
+it( 'tests that phrase matching matches phrase exactly.', function () {
 	// Adapter is loaded in the VIP Unit Test Case.
 	// Enable phrase matching.
 	elasticsearch_extensions()->load_adapter( Factory::vip_enterprise_search_adapter() );
 	elasticsearch_extensions()->enable_phrase_matching();
 
-	// WP_Query args.
-	$args = [
-		'ep_integrate'   => true,
-		'order'          => 'DESC',
-		'posts_per_page' => 10,
-		'post_type'      => 'post',
-		's'              => '"exact match"',
-	];
-
-	// Create the WP_Query instance to emulate search.
-	$search_query = new WP_Query( $args );
+	// Run a search for the phrase "exact match" and get the global wp_query.
+	$this->get( '/?s="exact match"' );
+	global $wp_query;
 
 	// Only 1 of the 3 posts should be found with hits for the words "exact match".
-	$this->assertEquals( 1, $search_query->found_posts );
+	$this->assertEquals( 1, $wp_query->found_posts );
+} );
 
-	// Disable phrase matching and test again.
+it( 'tests that disabling phrase matching disables the feature.', function () {
+	// Adapter is loaded in the VIP Unit Test Case.
+	// Enable phrase matching.
+	elasticsearch_extensions()->load_adapter( Factory::vip_enterprise_search_adapter() );
+	elasticsearch_extensions()->enable_phrase_matching();
+
+	// Disable the feature.
 	elasticsearch_extensions()->disable_phrase_matching();
-	$search_query = new WP_Query( $args );
-	// Expecting 2 posts to be found with hits for the words "exact match".
-	$this->assertEquals( 2, $search_query->found_posts );
+
+	$this->assertFalse( elasticsearch_extensions()->get_enable_phrase_matching() );
+} );
+
+it( 'tests that enabling phrase matching enables the feature.', function () {
+	// Adapter is loaded in the VIP Unit Test Case.
+	// Enable phrase matching.
+	elasticsearch_extensions()->load_adapter( Factory::vip_enterprise_search_adapter() );
+	elasticsearch_extensions()->enable_phrase_matching();
+
+	$this->assertTrue( elasticsearch_extensions()->get_enable_phrase_matching() );
 } );
